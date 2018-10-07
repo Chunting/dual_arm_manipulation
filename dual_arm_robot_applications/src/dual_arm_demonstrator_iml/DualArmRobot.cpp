74e7a80e554efe94a362cabe3c5d672e0bed3dc2
Chunting <jiaochunting@gmail.com>
2018-10-07 19:22:58 +0800

    change the std smart pointer to boost pointer
    This issue is related to the Ubuntu version, on Ubuntu 16, it should use std::make_shared, but on Ubuntu 14, it should use boost::make_shared instead.

--- dual_arm_robot_applications/src/dual_arm_demonstrator_iml/DualArmRobot.cpp
+++ dual_arm_robot_applications/src/dual_arm_demonstrator_iml/DualArmRobot.cpp
@@ -25,8 +25,7 @@ DualArmRobot::DualArmRobot(ros::NodeHandle& nh) :
     ur10_.setPlannerId("RRTConnectkConfigDefault");
 
     // planning scene monitor
-    // "robot_description" The name of the ROS parameter that contains the URDF (in string format) 
-    planning_scene_monitor_ = std::make_shared<planning_scene_monitor::PlanningSceneMonitor>("robot_description");
+    planning_scene_monitor_ = boost::make_shared<planning_scene_monitor::PlanningSceneMonitor>("robot_description");
 
     // initialize ur5 specific variables
     ur5_last_goal_pose_ = ur5_.getCurrentPose(ur5_.getEndEffectorLink());
